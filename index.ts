0d68062e8648aae4d3d5b6c216e68cbdf4a853df
Frank <frank.reimann@jambit.de>
2024-09-17 23:27:03 +0200

    .

--- index.ts
+++ index.ts
@@ -10,22 +10,64 @@ function $get<Z extends ZodSchema>(
   schema: Z,
   ...args: Parameters<typeof fetch>
 ): Promise<Z> {
-  return fetch(...args)
+  const [input, init] = args;
+  return fetch(input, {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+    ...init,
+  })
     .then((r) => r.json())
     .then(schema.parse);
 }
 
 const Pokemon = z.object({
   name: z.string(),
-  url: z.string(),
+  id: z.number(),
+  height: z.number(),
+  weight: z.number(),
+  base_experience: z.number(),
+  types: z.array(
+    z.object({
+      slot: z.number(),
+      type: z.object({
+        name: z.string(),
+        url: z.string(),
+      }),
+    })
+  ),
+  abilities: z.array(
+    z.object({
+      ability: z.object({
+        name: z.string(),
+        url: z.string(),
+      }),
+      is_hidden: z.boolean(),
+      slot: z.number(),
+    })
+  ),
+  sprites: z.object({
+    front_default: z.string().url().nullable(),
+    back_default: z.string().url().nullable(),
+    // Add other sprite fields as needed
+  }),
 });
 type Pokemon = z.infer<typeof Pokemon>;
+const res = await $get(Pokemon, "https://pokeapi.co/api/v2/pokemon/1");
 
-const res = $get(Pokemon, "api.pokemon.com");
+function isExpected<T>(val: T): T {
+  return val;
+}
 
-match(res).when(
-  (s) => s,
-  () => {
-    console.log("foo");
+function identity<T>(val: T): T {
+    return val;
   }
-);
+
+const isError = (e: unknown) => e instanceof Error;
+const mapMessage = (e: Error) => e.message;
+
+const mapped = match(res)
+  .when((s)=>s, identity)
+  .when(isError, mapMessage)
+  .otherwise(() => ({ error: "unexpected" }));
+
+console.log(mapped);
